3d53138afac8f438b2497f346ffb247b0720ed5c
Sierra <sierra.butler@bryanuniversity.edu>
2023-09-04 02:19:24 -0700

    saved it

--- week5/Capstone/MollysPlace/MollysPlace1/urls.py
+++ week5/Capstone/MollysPlace/MollysPlace1/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name = "index"),
+    path('home', views.home, name = "home"),
+    path('page1', views.page1, name = "page1"),
+    path('page2', views.page2, name = "page2"),
+    path('page3', views.page3, name = "page3"),
+    path('page4', views.page4, name = "page4"),
 ]
