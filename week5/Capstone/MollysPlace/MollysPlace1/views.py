5bb45caa083ced5a6a0b4084c5ca6a7ff689893f
Sierra <sierra.butler@bryanuniversity.edu>
2023-09-04 20:00:47 -0700

    oh crap

--- week5/Capstone/MollysPlace/MollysPlace1/views.py
+++ week5/Capstone/MollysPlace/MollysPlace1/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-    return render(request, 'home.html', {})
 
-def page1(request):
-    return render(request, 'page1.html', {})
 
-def page2(request):
-    return render(request, 'page2.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
 
-def page3(request):
-    return render(request, 'page3.html', {})
+# def page1(request):
+#     return render(request, 'page1.html', {})
 
-def page4(request):
-    return render(request, 'page4.html', {})
+# def page2(request):
+#     return render(request, 'page2.html', {})
+
+# def page3(request):
+#     return render(request, 'page3.html', {})
+
+# def page4(request):
+#     return render(request, 'page4.html', {})
+
+class HomeView(ListView):
+    model = Post 
+    template_name = 'home.html'
